e076d2ec0958649c83aec6dcb10da655188fc7e5
Alexey Komarov <q4arus@ya.ru>
2015-10-28 07:24:26 +0300

    add night sky terrain src

---
+++ soft/bsc/bsc/convert.sh
@@ -0,0 +1,6 @@
+pnmtopng xneg.pgm >> xneg.png
+pnmtopng xpos.pgm >> xpos.png
+pnmtopng yneg.pgm >> yneg.png
+pnmtopng ypos.pgm >> ypos.png
+pnmtopng zneg.pgm >> zneg.png
+pnmtopng zpos.pgm >> zpos.png
